c90f1d4db828a7d55f284053563bcbeeb3a3d747
ShuvalovPavel <p_a_v_e_l1995@mail.ru>
2018-01-15 21:10:53 +0300

    PUT, filename

--- src/main/java/com/mycompany/json_validator/Resource.java
+++ src/main/java/com/mycompany/json_validator/Resource.java
@@ -1,22 +1,24 @@
 package com.mycompany.json_validator;
 
-import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.BufferedReader;
 import java.util.stream.Collectors;
+import javax.ws.rs.PathParam;
 import org.json.*;
 
 
-/* Root resource */
+
 @Path("")
 
 /**
  * @author phn
  */
 public class Resource {
-    @POST
+    @PUT
+    @Path("/{file}")
     
     /**
      * Validate  json file
@@ -25,8 +27,7 @@ public class Resource {
      * 
      * @return String
      */
-    public String checkFile(InputStream stream){
-        Integer a;
+    public String checkFile(@PathParam("file") String file, InputStream stream){
         BufferedReader br = new BufferedReader(new InputStreamReader(stream));
         final String jsonInString = br.lines().collect(Collectors.joining("\n")) + "\n"; 
         String returnString = jsonInString;
@@ -43,7 +44,7 @@ public class Resource {
             " \"errorCode\"  : 12345,\n" +
             " \"errorMessage\" : \"" + errorMessage + "\",\n" +
             " \"errorPlace\" : \"" + errorPlace + "\",\n" +
-            " \"resource\"   : \"filename\",\n" +
+            " \"resource\"   : \"" + file + "\",\n" +
             " \"request-id\" : \"" + id + "\",\n" +
             "}";
         }
